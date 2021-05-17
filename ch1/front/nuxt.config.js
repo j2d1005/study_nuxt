844c748b3b09dc04fd031a7e6df2a728dc5d2290
Sam.Hong3 <sam.hon3@kakaocorp.com>
2021-05-17 15:14:31 +0900

    vuetify, axios 등 공통 모듈 nuxt에 선언하는 법

--- ch1/front/nuxt.config.js
+++ ch1/front/nuxt.config.js
@@ -2,5 +2,12 @@
 module.exports = {
   head: {
     title: 'NodeBird',
-  }
+  },
+  modules: [ // Vue에서 전역에 Vue.use(''); 선언하는것고 동일하다. nuxt에서는 여기다가 선언한다.
+    '@nuxtjs/vuetify',
+    '@nuxtjs/axios'
+  ],
+  vuetify: {
+
+  },
 }
