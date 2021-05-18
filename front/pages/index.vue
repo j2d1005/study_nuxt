710c5fa12bb5f28179406de7ff5764ccda03f43a
Sam.Hong3 <sam.hon3@kakaocorp.com>
2021-05-18 18:26:48 +0900

    더미데이터로 인피니트 스크롤

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -28,6 +28,36 @@ export default {
     mainPost() {
       return this.$store.state.posts.mainPosts;
     },
+    hasMorePost() {
+      return this.$store.state.posts.hasMorePost;
+    },
+  },
+  // fetch는 페이지가 마운트되기전에 실행된다.
+  // 마운트 전에 store에 데이터를 비동기적으로 넣을 때 사용된다.
+  fetch({ store }) {
+    store.dispatch("posts/loadPosts");
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll);
+  },
+  methods: {
+    onScroll() {
+      // window.scrollY - 현재 스크롤 높이
+      // document.documentElement.clientHeight - 브라우저 세로크기
+      // document.documentElement.scrollHeight - 페이지 전체 height
+      // window.scrollY + document.documentElement.clientHeight === document.documentElement.scrollHeight 라면 스크롤 최하단
+      if (
+        window.scrollY + document.documentElement.clientHeight >
+        document.documentElement.scrollHeight - 300
+      ) {
+        if (this.hasMorePost) {
+          this.$store.dispatch("posts/loadPosts");
+        }
+      }
+    },
   },
   head() {
     return {
--- front/store/posts.js
+++ front/store/posts.js
@@ -1,6 +1,10 @@
 export const state = () => ({
   mainPosts: [],
+  hasMorePost: true, // 게시물을 10개 단위로 불러올거고 10개 미만으로 게시물을 받아오게 되면 마지막 페이지이므로 false 로 바꾼다.
 });
+
+const totalPosts = 51;
+const limit = 10;
 
 export const mutations = {
   addMainPost(state, payload) {
@@ -14,6 +18,26 @@ export const mutations = {
     const index = state.mainPosts.findIndex((v) => v.id === payload.postId);
     state.mainPosts[index].Comments.unshift(payload);
   },
+  loadPosts(state, payload) {
+    const diff = totalPosts - state.mainPosts.length; // 아직 안 불려온 게시글 수
+
+    // Array(limit).fill()   -- limit만큼 빈 배열을 만든다.
+    // const fakePosts = Array(limit)
+    const fakePosts = Array(diff > limit ? limit : diff) // 남은 게시글이 10개보다 크면 10개 부르고 아니면 남은 갯수만큼 부른다.
+      .fill()
+      .map((v) => ({
+        id: Math.random().toString(),
+        User: {
+          id: 1,
+          nickname: "HongSam",
+        },
+        content: `Hello Infinite Scrolling - ${Math.random().toString()}`,
+        Comments: [],
+        Images: [],
+      }));
+    state.mainPosts = state.mainPosts.concat(fakePosts);
+    state.hasMorePost = fakePosts.length === limit;
+  },
 };
 
 export const actions = {
@@ -26,5 +50,10 @@ export const actions = {
   },
   addComment({ commit }, payload) {
     commit("addComment", payload);
+  },
+  loadPosts({ commit, state }, payload) {
+    if (state.hasMorePost) {
+      commit("loadPosts");
+    }
   },
 };
