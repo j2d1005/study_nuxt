0d815ac7444e20b75e6f54734c840a1aa423048d
Sam.Hong3 <sam.hon3@kakaocorp.com>
2021-05-23 15:03:21 +0900

    proxy 주석처리

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -12,11 +12,11 @@ module.exports = {
   ],
   vuetify: {},
 
-  axios: {
-    proxy: true,
-  },
-
-  proxy: {
-    "/api/": "http://localhost:3085/",
-  },
+  // axios: {
+  //   proxy: true,
+  // },
+  //
+  // proxy: {
+  //   "/api/": "http://localhost:3085/",
+  // },
 };
